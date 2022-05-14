89263b0eb1a5877ca168728f436778807aea3b4a
KITI-loubou <919070296@qq.com>
2022-05-15 00:54:04 +0800

    [fix] funcType

--- types/index.ts
+++ types/index.ts
@@ -4,9 +4,9 @@
  * @Author: VLOU
  * @Date: 2022-05-15 00:20:52
  * @LastEditors: VLOU
- * @LastEditTime: 2022-05-15 00:38:05
+ * @LastEditTime: 2022-05-15 00:52:59
  */
 
 export interface funcType {
-  [key: string]: any
+  [key: string]: Function
 }
