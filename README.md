1244b1bf46dbfa57f8ce087c9aae10582aba5a6b
VLOU <919070296@qq.com>
2023-02-09 16:27:41 +0800

    [update]更新文档说明，导出所有可用函数

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ npm i permission-file-type
 校验文件类型
 # 解决的痛点
   - 1.在文件上传的时候用户强行切换其他格式进行上传。利用文件类型校验能很好的给予提示并控制。
+
+# DOC
+
+| Function | Description |
+|   :---   |    :---:    |
+| verifyFormat(supportTypeAr, mime, name) |  supportTypeAr：['.jpg','.doc']，mine: fileMine, name: fileName |
+| is_docx(mime) | mine: fileMine, Function[is_docx, is_doc, is_pdf] |
+| is_image(mime, extName) | mine: fileMine, extName: fileExtName（文件后缀） |
--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@
  * @LastEditTime: 2022-05-15 00:33:26
  */
 
-import {functionObject as performObject} from './lib/is_type'
+import { functionObject as performObject } from './lib/is_type'
 import { imageAr } from './lib/supported'
 
 /**
@@ -16,53 +16,58 @@ import { imageAr } from './lib/supported'
  * @param {string} mine 'image/jpg'
  */
 
- function splitSupportTypeAr(supportTypeAr:string[], type?:string) {
-   const imgAr = []
-   const txtAr = []
- 
-   for (const extname of supportTypeAr) {
-     if (imageAr.includes(extname)) {
-       imgAr.push(extname)
-     } else {
-       txtAr.push(extname)
-     }
-   }
- 
-   return type === 'img' ? imgAr : txtAr
- }
- 
- export function verifyFormat(supportTypeAr:string[], mime:string, name:string) {
-   // 先判断mime类型
-   let type = ''
-   if (mime) {
-     type = mime.split('/')[1]
-   }
-   if (type && supportTypeAr.includes('.' + type) && type !== 'pdf') {
-     const supportAr = splitSupportTypeAr(supportTypeAr, 'img')
-     for (const extname of supportAr) {
-       if (performObject.is_image(mime, extname)) {
-         return true
-       }
-     }
-   } else if (type) {
-     const supportAr = splitSupportTypeAr(supportTypeAr)
-     for (const extname of supportAr) {
-       const funName = 'is_' + extname.split('.')[1]
-       if (performObject[funName] && performObject[funName](mime)) {
-         return true
-       }
-     }
-   }
- 
-   // 对于某些浏览器没有file.raw.type的处理
-   if (type === '') {
-     const funName = name.split('.')
-     type = funName[funName.length - 1] || ''
-     const supportAr = splitSupportTypeAr(supportTypeAr)
-     if (type && supportAr.includes('.' + type)) {
-       return true
-     }
-   }
- 
-   return false
- }
+function splitSupportTypeAr(supportTypeAr: string[], type?: string) {
+  const imgAr = []
+  const txtAr = []
+
+  for (const extname of supportTypeAr) {
+    if (imageAr.includes(extname)) {
+      imgAr.push(extname)
+    } else {
+      txtAr.push(extname)
+    }
+  }
+
+  return type === 'img' ? imgAr : txtAr
+}
+
+function verifyFormat(supportTypeAr: string[], mime: string, name: string) {
+  // 先判断mime类型
+  let type = ''
+  if (mime) {
+    type = mime.split('/')[1]
+  }
+  if (type && supportTypeAr.includes('.' + type) && type !== 'pdf') {
+    const supportAr = splitSupportTypeAr(supportTypeAr, 'img')
+    for (const extname of supportAr) {
+      if (performObject.is_image(mime, extname)) {
+        return true
+      }
+    }
+  } else if (type) {
+    const supportAr = splitSupportTypeAr(supportTypeAr)
+    for (const extname of supportAr) {
+      const funName = 'is_' + extname.split('.')[1]
+      if (performObject[funName] && performObject[funName](mime)) {
+        return true
+      }
+    }
+  }
+
+  // 对于某些浏览器没有file.raw.type的处理
+  if (type === '') {
+    const funName = name.split('.')
+    type = funName[funName.length - 1] || ''
+    const supportAr = splitSupportTypeAr(supportTypeAr)
+    if (type && supportAr.includes('.' + type)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+export default {
+  verifyFormat,
+  ...performObject
+}
